6387328974e8db8c3159e0b21334972983c45659
SunnFloww <butenkovarvara690@gmail.com>
2025-05-31 13:29:41 +0700

    Базовый интерфейс Входного окна

---
+++ KitchenApp/LoginForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace KitchenApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            loginPanel = new Panel();
+            stuffButton = new Button();
+            clientButton = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(clientButton);
+            panel1.Controls.Add(stuffButton);
+            panel1.Location = new Point(11, 14);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1215, 220);
+            panel1.TabIndex = 0;
+            // 
+            // loginPanel
+            // 
+            loginPanel.Location = new Point(11, 280);
+            loginPanel.Name = "loginPanel";
+            loginPanel.Size = new Size(1215, 370);
+            loginPanel.TabIndex = 1;
+            // 
+            // stuffButton
+            // 
+            stuffButton.Location = new Point(16, 36);
+            stuffButton.Name = "stuffButton";
+            stuffButton.Size = new Size(314, 150);
+            stuffButton.TabIndex = 0;
+            stuffButton.Text = "Сотрудник";
+            stuffButton.UseVisualStyleBackColor = true;
+            stuffButton.Click += stuffButton_Click;
+            // 
+            // clientButton
+            // 
+            clientButton.Location = new Point(880, 36);
+            clientButton.Name = "clientButton";
+            clientButton.Size = new Size(314, 150);
+            clientButton.TabIndex = 1;
+            clientButton.Text = "Клиент";
+            clientButton.UseVisualStyleBackColor = true;
+            clientButton.Click += clientButton_Click;
+            // 
+            // LoginForm
+            // 
+            ClientSize = new Size(1250, 680);
+            Controls.Add(loginPanel);
+            Controls.Add(panel1);
+            Name = "LoginForm";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button clientButton;
+        private Button stuffButton;
+        private Panel loginPanel;
+    }
+}
---
+++ KitchenApp/LoginForm.cs
@@ -0,0 +1,97 @@
+namespace KitchenApp
+{
+    public partial class LoginForm : Form
+    {
+        private TextBox phoneTextBox;
+        private Button getCodeButton;
+        private TextBox codeTextBox;
+        private Button clientLoginButton;
+
+        // Staff Login Controls
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private Button staffLoginButton;
+        private LinkLabel staffRegisterLink;
+        private LinkLabel staffForgotPassword;
+        public LoginForm()
+        {
+            InitializeComponent();
+            loginPanel.Visible = false; 
+        }
+        private void clientButton_Click(object sender, EventArgs e)
+        {
+            ShowClientLogin();    
+        }
+
+        private void stuffButton_Click(object sender, EventArgs e)
+        {
+            ShowStuffLogin();
+        }
+
+        private void ShowClientLogin()
+        {
+            ClearControls();
+
+            phoneTextBox = new TextBox { Width = 200, Height = 40, PlaceholderText = "Введите номер телефона" };
+            getCodeButton = new Button { Text = "Получить код", Height = 40, Width = 200 };
+            codeTextBox = new TextBox { Width = 200, Height = 40, PlaceholderText = "Введите код" };
+            clientLoginButton = new Button { Text = "Войти", Height = 40, Width = 200 };
+
+            clientLoginButton.Click += ClientLoginButton_Click;
+
+            loginPanel.Controls.Add(phoneTextBox);
+            loginPanel.Controls.Add(getCodeButton);
+            loginPanel.Controls.Add(codeTextBox);
+            loginPanel.Controls.Add(clientLoginButton);
+
+            phoneTextBox.Location = new Point(500, 10);
+            getCodeButton.Location = new Point(500, 40);
+            codeTextBox.Location = new Point(500, 90);
+            clientLoginButton.Location = new Point(500, 120);
+
+            loginPanel.Visible = true;
+        }
+        private void ShowStuffLogin()
+        {
+            ClearControls(); // Очищаем панель
+
+            usernameTextBox = new TextBox { Width = 200, PlaceholderText = "username" };
+            passwordTextBox = new TextBox { Width = 200, UseSystemPasswordChar = true, PlaceholderText = "Пароль" };
+            staffLoginButton = new Button { Text = "Войти", Width = 200, Height = 40, };
+            staffRegisterLink = new LinkLabel { Text = "Регистрация", ActiveLinkColor = Color.Orange, Width = 200, Height = 40 };
+            staffForgotPassword = new LinkLabel { Text = "Забыли пароль?", ActiveLinkColor = Color.Orange, Width = 200, Height = 40 };
+
+            staffLoginButton.Click += StaffLoginButton_Click;
+
+            // Добавляем элементы в панель
+            loginPanel.Controls.Add(usernameTextBox);
+            loginPanel.Controls.Add(passwordTextBox);
+            loginPanel.Controls.Add(staffLoginButton);
+            loginPanel.Controls.Add(staffRegisterLink);
+            loginPanel.Controls.Add(staffForgotPassword);
+
+            usernameTextBox.Location = new Point(500, 10);
+            passwordTextBox.Location = new Point(500, 50);
+            staffLoginButton.Location = new Point(500, 90);
+            staffRegisterLink.Location = new Point(543, 150);
+            staffForgotPassword.Location = new Point(530, 190);
+
+            // Делаем панель видимой
+            loginPanel.Visible = true;
+        }
+
+        private void ClearControls()
+        {
+            loginPanel.Controls.Clear();
+        }
+        private void ClientLoginButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Client Login Clicked!"); // Replace with your login logic
+        }
+
+        private void StaffLoginButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Staff Login Clicked!"); // Replace with your login logic
+        }
+    }
+}
